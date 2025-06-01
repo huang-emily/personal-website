2d0eef0a7d157b6d0c37f728331947f77cfdb5d0
huang-emily <ehuan015@gmail.com>
2025-06-01 02:03:22 -0700

    Fix up the content on About Me page

--- src/routes/credit/+page.svelte
+++ src/routes/credit/+page.svelte
@@ -2,10 +2,21 @@
 </script>
 
 <div class="text-center">
-    <h1 class="text-6xl p-4">Credits</h1>
-    <ul class="text-lg">
-        <li>Character Art by <a class="font-extrabold hover:underline" href="https://www.openpeeps.com/" rel="noopener noreferrer" target="_blank">Open Peeps</a></li>
-        <li>Vectors and icons by <a class="font-extrabold hover:underline" href="https://www.svgrepo.com" rel="noopener noreferrer" target="_blank">SVG Repo</a></li>
-        <li>Background Music by <a class="font-extrabold hover:underline" href="https://pmmusic.pro/downloads/" rel="noopener noreferrer" target="_blank">PM Music</a></li>
-    </ul>
+    <h1 class="text-5xl">Credits</h1>
+    <div class="my-5">
+        <h1 class="text-4xl">Assets</h1>
+        <ul class="text-lg">
+            <li>Character Art by <a class="font-extrabold hover:underline" href="https://www.openpeeps.com/" rel="noopener noreferrer" target="_blank">Open Peeps</a></li>
+            <li>Vectors and Icons by <a class="font-extrabold hover:underline" href="https://www.svgrepo.com" rel="noopener noreferrer" target="_blank">SVG Repo</a></li>
+            <li>Background Music by <a class="font-extrabold hover:underline" href="https://pmmusic.pro/downloads/" rel="noopener noreferrer" target="_blank">PM Music</a></li>
+        </ul>
+    </div>
+
+    <div class="my-5">
+        <h1 class="text-4xl">Inspiration</h1>
+        <ul class="text-lg">
+            <li>Page Structure from <a class="font-extrabold hover:underline" href="https://www.sharyap.com/" rel="noopener noreferrer" target="_blank">Sharlene Yap's Website</a></li>
+            <li>Theme from <a class="font-extrabold hover:underline" href="https://animalcrossing.nintendo.com/new-horizons/" rel="noopener noreferrer" target="_blank">Animal Crossing: New Horizons</a></li>
+        </ul>
+    </div>
 </div>
