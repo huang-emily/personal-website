55ba44eae98d941bc6cadd5d586e4a5b4f612ae4
huang-emily <ehuan015@gmail.com>
2025-06-01 22:33:52 -0700

    Change credit page entry and add tab title and icon

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="bottom-0 text-center p-2">
-    <p class="opacity-50 text-[0.7rem] sm:text-xs font-medium">made with Svelte, TailwindCSS, and <a class="font-extrabold hover:underline" href="/credit">lots of awesome resources</a>!</p>
+    <p class="opacity-50 text-[0.7rem] sm:text-xs font-medium">©2025 Emily Huang</p>
 </div>
 
 <style lang="postcss">
--- src/routes/credit/+page.svelte
+++ src/routes/credit/+page.svelte
@@ -5,9 +5,10 @@
 <div class="flex flex-col text-center">
     <!-- credits text -->
     <h1 class="text-6xl">Credits</h1>
-    <div class="grid grid-cols-1 lg:grid-cols-2">
+    <p class="text-sm w-sm">This website would not be possible without these wonderful assets and sources of inspiration!</p>
+    <div class="grid grid-cols-1">
         <div class="my-5">
-            <h1 class="text-4xl">Assets</h1>
+            <h1 class="text-5xl">Assets</h1>
             <ul class="text-md">
                 <li>Character Art by <a class="font-extrabold hover:underline" href="https://www.openpeeps.com/" rel="noopener noreferrer" target="_blank">Open Peeps</a></li>
                 <li>Vectors and Icons by <a class="font-extrabold hover:underline" href="https://www.svgrepo.com" rel="noopener noreferrer" target="_blank">SVG Repo</a></li>
@@ -16,7 +17,7 @@
         </div>
 
         <div class="my-5">
-            <h1 class="text-4xl">Inspiration</h1>
+            <h1 class="text-5xl">Inspiration</h1>
             <ul class="text-md">
                 <li>Page Structure from <a class="font-extrabold hover:underline" href="https://www.sharyap.com/" rel="noopener noreferrer" target="_blank">Sharlene Yap's Website</a></li>
                 <li>Theme from <a class="font-extrabold hover:underline" href="https://animalcrossing.nintendo.com/new-horizons/" rel="noopener noreferrer" target="_blank">Animal Crossing: New Horizons</a></li>
